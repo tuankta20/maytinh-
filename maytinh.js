b9f1b456ceba9f4faac6e394dd872a5cd11d53b3
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-08-05 10:22:01 +0700

    Calculate

--- maytinh.js
+++ maytinh.js
@@ -1,14 +1,16 @@
 function addNumber(number) {
-    document.getElementById("hien_thi").value += number;
-    
+    document.getElementById("display").value += number;
 }
 function calculate(a) {
-    let number = document.getElementById("hien_thi").value;
-    document.getElementById("hien_thi").value = number + a
+    let number = document.getElementById("display").value;
+    document.getElementById("display").value = number + a
 }
 function result() {
-    let output = document.getElementById("hien_thi").value;
+    let output = document.getElementById("display").value;
     let result = eval(output);
-    document.getElementById("hien_thi").value = result;
+    document.getElementById("display").value = result;
+
+}function clean(){
+    document.getElementById("display").value=0
 
 }
